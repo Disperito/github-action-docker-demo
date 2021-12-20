ae1ac8363a472ae7a5a04dab629f2f37da7d4807
Disperito <dmwqaq@foxmail.com>
2021-12-20 15:33:27 +0800

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Docker is easy
+          Docker & Docker compose is easy
         </p>
         <a
           className="App-link"
